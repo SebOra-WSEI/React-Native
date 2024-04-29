35a9a46b0c608316266b3a62f8721f2c1d6785e7
SebOra-WSEI <LiBroPWSZ@gmail.com>
2024-04-29 18:09:15 +0200

    Add modal content

--- rick-and-morty-app/src/app/modal.tsx
+++ rick-and-morty-app/src/app/modal.tsx
@@ -1,17 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { Platform, StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/src/components/EditScreenInfo';
 import { Text, View } from '@/src/components/Themed';
 
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+      <Text style={styles.title}>College of Economics and Computer Science</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/modal.tsx" />
-
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
+      <Text style={styles.text}>Mobile application programming</Text>
+      <Text style={styles.text}>Sebastian Oraczek</Text>
+      <Text style={styles.text}>May 2024</Text>
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
   );
@@ -24,12 +22,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {
-    fontSize: 20,
+    fontSize: 22,
     fontWeight: 'bold',
+    textAlign: 'center',
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  text: {
+    fontSize: 13,
+    lineHeight: 21,
   },
 });
