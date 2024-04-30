2fb3c2fbbfbc49e7ac9a8db47a4623fbf5dabfc3
SebOra-WSEI <LiBroPWSZ@gmail.com>
2024-04-30 15:49:07 +0200

    create

--- rick-and-morty-app/src/hooks/useGetCharacters.ts
+++ rick-and-morty-app/src/hooks/useGetCharacters.ts
@@ -1,11 +1,10 @@
 import { useEffect, useState } from 'react';
-import { Character, CharacterFilters } from '../types/character';
+import { Character } from '../types/character';
 import { CharacterResponse, QueryResponse } from '../types/response';
 import { ApiURL } from '../constants/api';
 
 export const useGetCharacters = (
-  currentPage: number,
-  filter: CharacterFilters | null
+  currentPage: number
 ): QueryResponse<Array<Character>> => {
   const [characters, setCharacters] = useState<Array<Character>>([]);
   const [loading, setLoading] = useState<boolean>(true);
@@ -13,14 +12,11 @@ export const useGetCharacters = (
   const [hasNextPage, setHasNextPage] = useState<boolean>(true);
 
   const fetchData = async (page: number) => {
-    await fetch(`${ApiURL}?page=${page}${!!filter ? `&${filter}=morty` : ''}`)
+    await fetch(`${ApiURL}?page=${page}`)
       .then((res) => res.json())
       .then((res: CharacterResponse) => {
         setHasNextPage(!!res.info.next);
-        setCharacters([
-          ...filterCharacters(characters, filter),
-          ...res.results,
-        ]);
+        setCharacters([...characters, ...res.results]);
         setLoading(false);
       })
       .catch((err) => {
@@ -31,7 +27,7 @@ export const useGetCharacters = (
 
   useEffect(() => {
     fetchData(currentPage);
-  }, [currentPage, filter]);
+  }, [currentPage]);
 
   return {
     loading,
@@ -40,14 +36,3 @@ export const useGetCharacters = (
     hasNextPage,
   };
 };
-
-function filterCharacters(
-  characters: Array<Character>,
-  filter: CharacterFilters | null
-) {
-  return characters.filter((c) =>
-    filter
-      ? c[filter].toLocaleLowerCase().includes('morty'.toLocaleLowerCase())
-      : c
-  );
-}
