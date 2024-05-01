0280718cc06acd37b29ba122d507cfb9181dd23b
SebOra-WSEI <LiBroPWSZ@gmail.com>
2024-05-01 13:27:42 +0200

    create hook

--- rick-and-morty-app/src/app/[id].tsx
+++ rick-and-morty-app/src/app/[id].tsx
@@ -6,24 +6,27 @@ import { Character } from "../types/character";
 import { endpoints } from "../utils/endpoints";
 import { listStyles } from "../styles/listStyles";
 import { UnknownError } from "../components/Error/UnknownError";
+import { useGetDataById } from "../hooks/useGetDataById";
 
 
 export default function CharacterDetails() {
-
   const { id } = useLocalSearchParams();
 
+  const { loading, error, data } =
+    useGetDataById<Character>(endpoints.characters, id as string);
 
-  // if (loading) {
-  //   return (
-  //     <View style={listStyles.loader}>
-  //       <ActivityIndicator size='large' />
-  //     </View>
-  //   );
-  // }
 
-  // if (error) {
-  //   return <UnknownError />;
-  // }
+  if (loading) {
+    return (
+      <View style={listStyles.loader}>
+        <ActivityIndicator size='large' />
+      </View>
+    );
+  }
+
+  if (error) {
+    return <UnknownError />;
+  }
 
   return (
     <View>
