4375449f1f8a31d015b3e0bc34d7598542aaa6b5
SebOra-WSEI <LiBroPWSZ@gmail.com>
2024-05-02 09:00:56 +0200

    refactor modal

--- rick-and-morty-app/src/components/FilterModal/FilterModal.tsx
+++ rick-and-morty-app/src/components/FilterModal/FilterModal.tsx
@@ -1,17 +1,24 @@
-import { View, Modal, StyleSheet } from 'react-native';
+import { View, Modal, StyleSheet, Pressable, Text } from 'react-native';
 import React, { PropsWithChildren } from 'react';
 
 interface FilterModalProps extends PropsWithChildren {
   isModalVisible: boolean;
+  handleOnPress: () => void;
 }
 
 export const FilterModal: React.FC<FilterModalProps> = ({
   isModalVisible,
+  handleOnPress,
   children,
 }) => (
   <Modal animationType='slide' transparent visible={isModalVisible}>
     <View style={styles.centeredView}>
-      <View style={styles.modalView}>{children}</View>
+      <View style={styles.modalView}>
+        {children}
+        <Pressable style={styles.button} onPress={handleOnPress}>
+          <Text style={styles.textStyle}>Filter</Text>
+        </Pressable>
+      </View>
     </View>
   </Modal>
 );
@@ -34,5 +41,17 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    marginTop: 20,
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
